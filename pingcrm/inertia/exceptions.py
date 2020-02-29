cd9a174198739fbc4f6dd566b6fd4c8897e71cbf
Rory Casey <>
2020-02-29 15:20:20 +1300

    Ensure the exception handler only invokes on inertia requests

--- pingcrm/inertia/exceptions.py
+++ pingcrm/inertia/exceptions.py
@@ -4,7 +4,6 @@ from rest_framework.exceptions import ValidationError, APIException, PermissionD
 
 
 LOGIN_REDIRECT="/login" # getattr(settings, 'INERTIA_LOGIN_REDIRECT', '/login')
-logger = logging.getLogger('django')
 
 
 class Conflict(APIException):
@@ -20,13 +19,16 @@ class Conflict(APIException):
 def inertia_exception_handler(exc, context):
     override_status = None
     override_headers = {}
-    if isinstance(exc, ValidationError):
-        logger.info("Got a validation error")
+
+    request = context['request']
+    is_inertia = hasattr(request, 'inertia')
+
+    if is_inertia and isinstance(exc, ValidationError):
         context['request'].session['errors'] = exc.detail
         override_headers["Location"] = context['request'].path
         override_status = status.HTTP_302_FOUND
 
-    if isinstance(exc, PermissionDenied) or isinstance(exc, NotAuthenticated):
+    if is_inertia and (isinstance(exc, PermissionDenied) or isinstance(exc, NotAuthenticated)):
         override_status = status.HTTP_302_FOUND
         override_headers["Location"] = LOGIN_REDIRECT
 
@@ -36,12 +38,10 @@ def inertia_exception_handler(exc, context):
     if override_status:
         response.status_code = override_status
 
-    if response.status_code == 409:
+    if is_inertia and response.status_code == 409:
         response['X-Inertia-Location'] = context['request'].path
 
     for header in override_headers:
         response[header] = override_headers[header]
-
-    logger.info(override_headers)
 
     return response
